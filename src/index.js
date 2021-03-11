ecf9a224b5f67d21cda918a6cd29a7fee3512826
Alex <alex-88_90@mail.ru>
2021-03-11 22:16:39 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array !=[] && array !=Infinity) {
+    return Math.min(...array);
+
+  } else {
+    return 0;
+  }
+
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array !=[] && array !=Infinity) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array !=[] && array !=Infinity) {
+    return array.reduce(function (a, b) {
+  return a + b;
+     }) / array.length;
+  } else {
+    return 0;
+  }
+
 }
